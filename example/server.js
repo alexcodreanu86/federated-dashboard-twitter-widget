6663d0e9ed3be29bed809c82be2c15e72ad01ca8
Alex Codreanu <alexcodreanu86@gmail.com>
2014-06-25 14:13:41 -0500

    added example folder

--- example/server.js
+++ example/server.js
@@ -9,7 +9,7 @@
 
   util = require('util');
 
-  Twitter = require('./.tmp/scripts/backEnd/twitter/api');
+  Twitter = require('./bower_components/twitter-widget/dist/backend_module');
 
   app = express();
 
@@ -36,41 +36,12 @@
 
   app.get('/', function(request, response) {
     return response.render('index', {
-      title: "Federated dashboard"
+      title: "Twitter Widget"
     });
   });
 
   server = app.listen(5000, function() {
     return console.log("listening on port " + (server.address().port));
   });
-
-}).call(this);
-
-(function() {
-  var Twitter, twitter;
-
-  twitter = require('twitter');
-
-  Twitter = (function() {
-    function Twitter() {}
-
-    Twitter.setupAuthenticationObject = function() {
-      return new twitter({
-        consumer_key: process.env.TWITTER_API_KEY,
-        consumer_secret: process.env.TWITTER_API_SECRET,
-        access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-      });
-    };
-
-    Twitter.getSearchFormat = function(searchString) {
-      return "" + searchString + " OR #" + searchString;
-    };
-
-    return Twitter;
-
-  })();
-
-  module.exports = Twitter;
 
 }).call(this);
